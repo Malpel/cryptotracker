cd1c75d5f69ce4f6c4ee249b84870c6f4b1d9853
Malpel <m.a.pelkonen2@gmail.com>
2018-03-01 15:59:13 +0200

    added data fetching

--- src/Actions/FetchCoinData.js
+++ src/Actions/FetchCoinData.js
@@ -1,10 +1,24 @@
 import axios from 'axios';
+import { FETCHING_COIN_DATA, 
+    FETCHING_COIN_DATA_SUCCESS, 
+    FETCHING_COIN_DATA_FAILURE 
+} from './../Utils/ActionTypes';
 
-const apiBaseUrl = 'https://api.coinmarketcap.com';
+
+const apiBaseUrl = 'https://api.coinmarketcap.com/';
+
 
 // this is a thunk action
 export default function fetchCoinData() {
     return dispatch => {
         dispatch({ type: FETCHING_COIN_DATA })
+
+        return axios.get(apiBaseUrl + 'v1/ticker/bitcoin/')
+            .then((response) =>  {
+                dispatch({ type: FETCHING_COIN_DATA_SUCCESS, payload: response.data })
+            })
+            .catch((error) => {
+                dispatch({ type: FETCHING_COIN_DATA_FAILURE, payload: error.data })
+            });
     }
 }
